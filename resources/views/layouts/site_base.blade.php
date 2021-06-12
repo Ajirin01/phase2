be19a27b83ec1310ad75fa610089bb4f75b35a3a
Ajirin <mubarakolagoke@gmail.com>
2021-06-12 16:03:02 +0100

    admin included in the files

--- resources/views/layouts/site_base.blade.php
+++ resources/views/layouts/site_base.blade.php
@@ -483,57 +483,57 @@
     </div>
 
 
-    <!-- Quick view modal start -->
-    {{-- <div class="modal" id="quick_view">
-        <div class="modal-dialog modal-lg modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <!-- product details inner end -->
-                    <div class="product-details-inner">
-                        <div class="row">
-                            <div class="col-lg-5">
-                                <div class="product-large-slider slick-arrow-style_2 mb-20">
-                                    <div class="pro-large-img">
-                                        <img src="{{ asset('site/assets/img/product/product-details-img1.jpg') }}" alt="" />
+        <!-- Quick view modal start -->
+        {{-- <div class="modal" id="quick_view">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- product details inner end -->
+                        <div class="product-details-inner">
+                            <div class="row">
+                                <div class="col-lg-5">
+                                    <div class="product-large-slider slick-arrow-style_2 mb-20">
+                                        <div class="pro-large-img">
+                                            <img src="{{ asset('site/assets/img/product/product-details-img1.jpg') }}" alt="" />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-7">
-                                <div class="product-details-des mt-md-34 mt-sm-34">
-                                    <div class="availability mt-10">
-                                        <h5>Availability:</h5>
-                                        <span>1 in stock</span>
-                                    </div>
-                                    <div class="pricebox">
-                                        <span class="regular-price">#160.00</span>
-                                    </div>
-                                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
-                                        Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea
-                                        dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris
-                                        consequat nisi ut mauris efficitur lacinia.</p>
-                                    <div class="quantity-cart-box d-flex align-items-center mt-20">
-                                        <div class="quantity">
-                                            <div class="pro-qty"><input type="text" value="1"></div>
+                                <div class="col-lg-7">
+                                    <div class="product-details-des mt-md-34 mt-sm-34">
+                                        <div class="availability mt-10">
+                                            <h5>Availability:</h5>
+                                            <span>1 in stock</span>
                                         </div>
-                                        <div class="action_link">
-                                            <a class="buy-btn" href="#">add to cart<i class="fa fa-shopping-cart"></i>
-                                            </a>
+                                        <div class="pricebox">
+                                            <span class="regular-price">#160.00</span>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.<br>
+                                            Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea
+                                            dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris
+                                            consequat nisi ut mauris efficitur lacinia.</p>
+                                        <div class="quantity-cart-box d-flex align-items-center mt-20">
+                                            <div class="quantity">
+                                                <div class="pro-qty"><input type="text" value="1"></div>
+                                            </div>
+                                            <div class="action_link">
+                                                <a class="buy-btn" href="#">add to cart<i class="fa fa-shopping-cart"></i>
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <!-- product details inner end -->
                     </div>
-                    <!-- product details inner end -->
                 </div>
             </div>
-        </div>
-    </div> --}}
-    <!-- Quick view modal end -->
+        </div> --}}
+        <!-- Quick view modal end -->
 
     <!-- Scroll to top start -->
     <div class="scroll-top not-visible">
