373e6e54a58edcf766f1c34b92c21fe535f945ca
Ajirin <mubarakolagoke@gmail.com>
2021-10-14 23:31:08 +0100

    receipt print fixed

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -353,7 +353,22 @@
         <!-- /.info-box -->
       </div> --}}
       <!-- /.col -->
-      <div class="col-12 col-sm-6 col-md-4">
+      <div class="col-12 col-sm-6 col-md-3">
+        <div class="info-box mb-3">
+          <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+          <div class="info-box-content">
+            <span class="info-box-text">Products</span>
+            <span class="info-box-number">41,410</span>
+          </div>
+          <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+      </div>
+      <!-- /.col -->
+
+      <!-- /.col -->
+      <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box mb-3">
           <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
@@ -370,7 +385,7 @@
       <!-- fix for small devices only -->
       <div class="clearfix hidden-md-up"></div>
 
-      <div class="col-12 col-sm-6 col-md-4">
+      <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box mb-3">
           <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
@@ -383,7 +398,7 @@
         <!-- /.info-box -->
       </div>
       <!-- /.col -->
-      <div class="col-12 col-sm-6 col-md-4">
+      <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box mb-3">
           <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
@@ -429,19 +444,19 @@
           <!-- /.card-header -->
           <div class="card-body">
             <div class="row">
-              <div class="col-md-8">
+              <div class="col-md-12">
                 <p class="text-center">
                   <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
                 </p>
 
                 <div class="chart">
                   <!-- Sales Chart Canvas -->
-                  <canvas id="salesChart" height="180" style="height: 180px;"></canvas>
+                  <canvas id="salesChart" height="180" style="height: 300px;"></canvas>
                 </div>
                 <!-- /.chart-responsive -->
               </div>
               <!-- /.col -->
-              <div class="col-md-4">
+              {{-- <div class="col-md-4">
                 <p class="text-center">
                   <strong>Goal Completion</strong>
                 </p>
@@ -481,13 +496,13 @@
                   </div>
                 </div>
                 <!-- /.progress-group -->
-              </div>
+              </div> --}}
               <!-- /.col -->
             </div>
             <!-- /.row -->
           </div>
           <!-- ./card-body -->
-          <div class="card-footer">
+          {{-- <div class="card-footer">
             <div class="row">
               <div class="col-sm-3 col-6">
                 <div class="description-block border-right">
@@ -526,7 +541,7 @@
               </div>
             </div>
             <!-- /.row -->
-          </div>
+          </div> --}}
           <!-- /.card-footer -->
         </div>
         <!-- /.card -->
@@ -562,7 +577,7 @@
                   <th>Order ID</th>
                   <th>Item</th>
                   <th>Status</th>
-                  <th>Popularity</th>
+                  {{-- <th>Popularity</th> --}}
                 </tr>
                 </thead>
                 <tbody>
@@ -571,57 +586,57 @@
                   <td><a href="pages/examples/invoice.html">OR9842</a></td>
                   <td>Call of Duty IV</td>
                   <td><span class="badge badge-success">Shipped</span></td>
-                  <td>
+                  {{-- <td>
                     <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
-                  </td>
+                  </td> --}}
                 </tr>
                 <tr>
                   <td><a href="pages/examples/invoice.html">OR1848</a></td>
                   <td>Samsung Smart TV</td>
                   <td><span class="badge badge-warning">Pending</span></td>
-                  <td>
+                  {{-- <td>
                     <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
-                  </td>
+                  </td> --}}
                 </tr>
                 <tr>
                   <td><a href="pages/examples/invoice.html">OR7429</a></td>
                   <td>iPhone 6 Plus</td>
                   <td><span class="badge badge-danger">Delivered</span></td>
-                  <td>
+                  {{-- <td>
                     <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
-                  </td>
+                  </td> --}}
                 </tr>
                 <tr>
                   <td><a href="pages/examples/invoice.html">OR7429</a></td>
                   <td>Samsung Smart TV</td>
                   <td><span class="badge badge-info">Processing</span></td>
-                  <td>
+                  {{-- <td>
                     <div class="sparkbar" data-color="#00c0ef" data-height="20">90,80,-90,70,-61,83,63</div>
-                  </td>
+                  </td> --}}
                 </tr>
                 <tr>
                   <td><a href="pages/examples/invoice.html">OR1848</a></td>
                   <td>Samsung Smart TV</td>
                   <td><span class="badge badge-warning">Pending</span></td>
-                  <td>
+                  {{-- <td>
                     <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
-                  </td>
+                  </td> --}}
                 </tr>
                 <tr>
                   <td><a href="pages/examples/invoice.html">OR7429</a></td>
                   <td>iPhone 6 Plus</td>
                   <td><span class="badge badge-danger">Delivered</span></td>
-                  <td>
+                  {{-- <td>
                     <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
-                  </td>
+                  </td> --}}
                 </tr>
                 <tr>
                   <td><a href="pages/examples/invoice.html">OR9842</a></td>
                   <td>Call of Duty IV</td>
                   <td><span class="badge badge-success">Shipped</span></td>
-                  <td>
+                  {{-- <td>
                     <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
-                  </td>
+                  </td> --}}
                 </tr>
                 </tbody>
               </table>
@@ -630,7 +645,7 @@
           </div>
           <!-- /.card-body -->
           <div class="card-footer clearfix">
-            <a href="javascript:void(0)" class="btn btn-sm btn-info float-left">Place New Order</a>
+            {{-- <a href="javascript:void(0)" class="btn btn-sm btn-info float-left">Place New Order</a> --}}
             <a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All Orders</a>
           </div>
           <!-- /.card-footer -->
